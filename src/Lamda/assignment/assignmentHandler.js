9ebecb13f74d6ed52c9b77dc78a35d48cc054508
surendra <surendra.chakali@hyniva.com>
2024-03-27 12:05:34 +0530

    changes

--- src/Lamda/assignment/assignmentHandler.js
+++ src/Lamda/assignment/assignmentHandler.js
@@ -69,6 +69,8 @@ const updateAssignment = async (event) => {
           }),
           {
             ":updatedDateTime": formattedDate,
+            ":role": requestBody.role.toLowerCase(),
+            ":designation": requestBody.designation.toLowerCase(),
           }
         )
       ),
