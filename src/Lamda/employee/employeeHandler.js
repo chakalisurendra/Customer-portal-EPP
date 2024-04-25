95007570ab30c548d4c96231b1a9004a202e063f
surendra <surendra.chakali@hyniva.com>
2024-04-25 19:25:23 +0530

    get by role

--- src/Lamda/employee/employeeHandler.js
+++ src/Lamda/employee/employeeHandler.js
@@ -1,26 +1,11 @@
-const {
-  DynamoDBClient,
-  PutItemCommand,
-  UpdateItemCommand,
-  DeleteItemCommand,
-  GetItemCommand,
-  ScanCommand,
-} = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, PutItemCommand, UpdateItemCommand, DeleteItemCommand, GetItemCommand, ScanCommand } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 const moment = require("moment");
 const client = new DynamoDBClient();
-const {
-  validateEmployeeDetails,
-  validateUpdateEmployeeDetails,
-} = require("../../validator/validateRequest");
+const { validateEmployeeDetails, validateUpdateEmployeeDetails } = require("../../validator/validateRequest");
 const { autoIncreamentId, pagination } = require("../../utils/comman");
-const {
-  updateEmployeeAllowedFields,
-} = require("../../validator/validateFields");
-const {
-  httpStatusCodes,
-  httpStatusMessages,
-} = require("../../environment/appconfig");
+const { updateEmployeeAllowedFields } = require("../../validator/validateFields");
+const { httpStatusCodes, httpStatusMessages } = require("../../environment/appconfig");
 const currentDate = Date.now();
 const formattedDate = moment(currentDate).format("MM-DD-YYYY HH:mm:ss");
 
@@ -36,9 +21,7 @@ const createEmployee = async (event) => {
     const requestBody = JSON.parse(event.body);
 
     const validationResponse = validateEmployeeDetails(requestBody);
-    console.log(
-      `valdation : ${validationResponse.validation} message: ${validationResponse.validationMessage} `
-    );
+    console.log(`valdation : ${validationResponse.validation} message: ${validationResponse.validationMessage} `);
 
     if (!validationResponse.validation) {
       console.log(validationResponse.validationMessage);
@@ -54,10 +37,7 @@ const createEmployee = async (event) => {
       console.log("Email address already exists.");
       throw new Error("Email address already exists.");
     }
-    const newEmployeeId = await autoIncreamentId(
-      process.env.EMPLOYEE_TABLE,
-      "employeeId"
-    );
+    const newEmployeeId = await autoIncreamentId(process.env.EMPLOYEE_TABLE, "employeeId");
     console.log("new employee id : ", newEmployeeId);
     const params = {
       TableName: process.env.EMPLOYEE_TABLE,
@@ -103,10 +83,7 @@ const createEmployee = async (event) => {
     if (requestBody.branchOffice === "San Antonio, USA") {
       onsite = "Yes";
     }
-    const newAssignmentId = await autoIncreamentId(
-      process.env.ASSIGNMENTS_TABLE,
-      "assignmentId"
-    );
+    const newAssignmentId = await autoIncreamentId(process.env.ASSIGNMENTS_TABLE, "assignmentId");
     const assignmentParams = {
       TableName: process.env.ASSIGNMENTS_TABLE,
       Item: marshall({
@@ -125,9 +102,7 @@ const createEmployee = async (event) => {
         updatedDateTime: null,
       }),
     };
-    const createAssignmentResult = await client.send(
-      new PutItemCommand(assignmentParams)
-    );
+    const createAssignmentResult = await client.send(new PutItemCommand(assignmentParams));
     response.body = JSON.stringify({
       message: httpStatusMessages.SUCCESSFULLY_CREATED_EMPLOYEE_DETAILS,
       data: {
@@ -160,8 +135,7 @@ const updateEmployee = async (event) => {
     const requestBody = JSON.parse(event.body);
     console.log("Request Body:", requestBody);
     //const { employeeId } = event.queryStringParameters;
-    const employeeId =
-      event.queryStringParameters && event.queryStringParameters.employeeId;
+    const employeeId = event.queryStringParameters && event.queryStringParameters.employeeId;
 
     if (!employeeId) {
       console.log("Employee Id is required");
@@ -182,14 +156,10 @@ const updateEmployee = async (event) => {
       return response;
     }
 
-    const objKeys = Object.keys(requestBody).filter((key) =>
-      updateEmployeeAllowedFields.includes(key)
-    );
+    const objKeys = Object.keys(requestBody).filter((key) => updateEmployeeAllowedFields.includes(key));
     console.log(`Employee with objKeys ${objKeys} `);
     const validationResponse = validateUpdateEmployeeDetails(requestBody);
-    console.log(
-      `valdation : ${validationResponse.validation} message: ${validationResponse.validationMessage} `
-    );
+    console.log(`valdation : ${validationResponse.validation} message: ${validationResponse.validationMessage} `);
 
     if (!validationResponse.validation) {
       console.log(validationResponse.validationMessage);
@@ -200,10 +170,7 @@ const updateEmployee = async (event) => {
       return response;
     }
 
-    const officialEmailIdExists = await isEmailNotEmployeeIdExists(
-      requestBody.officialEmailId,
-      employeeId
-    );
+    const officialEmailIdExists = await isEmailNotEmployeeIdExists(requestBody.officialEmailId, employeeId);
     if (officialEmailIdExists) {
       console.log("officialEmailId already exists.");
       response.statusCode = 400;
@@ -331,12 +298,10 @@ const getAllEmployees = async (event) => {
   let branchFilter = [];
 
   if (event.multiValueQueryStringParameters && event.multiValueQueryStringParameters.designation) {
-    designationFilter = event.multiValueQueryStringParameters.designation
-      .flatMap((designation) => designation.split(",")); // Split by commas if exists
+    designationFilter = event.multiValueQueryStringParameters.designation.flatMap((designation) => designation.split(",")); // Split by commas if exists
   }
   if (event.multiValueQueryStringParameters && event.multiValueQueryStringParameters.branchOffice) {
-    branchFilter = event.multiValueQueryStringParameters.branchOffice
-      .flatMap((branchOffice) => branchOffice.split(",")); // Split by commas if exists
+    branchFilter = event.multiValueQueryStringParameters.branchOffice.flatMap((branchOffice) => branchOffice.split(",")); // Split by commas if exists
   }
 
   try {
@@ -358,12 +323,7 @@ const getAllEmployees = async (event) => {
 
     // Apply filters if provided
     if (designationFilter.length > 0 || branchFilter.length > 0) {
-      console.log(
-        "Filtering started with designationFilter:",
-        designationFilter,
-        "and branchFilter:",
-        branchFilter
-      );
+      console.log("Filtering started with designationFilter:", designationFilter, "and branchFilter:", branchFilter);
       filteredItems = applyFilters(filteredItems, designationFilter, branchFilter);
       if (filteredItems.length === 0) {
         throw new Error("No employees found matching the filter criteria.");
@@ -372,7 +332,11 @@ const getAllEmployees = async (event) => {
     }
 
     // Apply pagination
-    const paginatedData = pagination(filteredItems.map((item) => unmarshall(item)), pageNo, pageSize);
+    const paginatedData = pagination(
+      filteredItems.map((item) => unmarshall(item)),
+      pageNo,
+      pageSize
+    );
 
     if (!paginatedData.items || paginatedData.items.length === 0) {
       console.log("No employees found after filtering.");
@@ -400,20 +364,19 @@ const getAllEmployees = async (event) => {
 
 const applySearchCriteria = (employeesData, searchText) => {
   console.log("Applying search criteria:", searchText);
-  return employeesData.filter(employee => matchesSearchText(employee, searchText));
+  return employeesData.filter((employee) => matchesSearchText(employee, searchText));
 };
 
 const applyFilters = (employeesData, designationFilter, branchFilter) => {
   console.log("Applying filters...");
-  return employeesData.filter(employee => {
+  return employeesData.filter((employee) => {
     // Check if employee.branch exists before accessing its properties
     if (!employee.branchOffice || !employee.branchOffice.S || !employee.designation || !employee.designation.S) {
       // If employee data is incomplete, skip filtering for this employee
       return false;
     }
     console.log("Employee:", employee);
-    const passesDesignationFilter = designationFilter.length === 0 ||
-      (employee.designation && designationFilter.includes(employee.designation.S));
+    const passesDesignationFilter = designationFilter.length === 0 || (employee.designation && designationFilter.includes(employee.designation.S));
     // Note: Use `.S` to access the string value of DynamoDB attributes
     const passesBranchFilter = branchFilter.length === 0 || matchesBranch(employee.branchOffice.S, branchFilter);
     const passesFilters = passesDesignationFilter && passesBranchFilter;
@@ -428,16 +391,14 @@ const matchesSearchText = (employee, searchText) => {
   }
   const name = employee.name ? employee.name.S.toLowerCase() : "";
   //const employeeId = employee.employeeId ? employee.employeeId.N.toString() : ""; // Convert to string for comparison
-  return (
-    name.includes(searchText.toLowerCase())
+  return name.includes(searchText.toLowerCase());
   //  || employeeId === searchText
-  );
 };
 
 const matchesBranch = (employeeBranch, branchFilter) => {
   for (const filter of branchFilter) {
-    const phrases = filter.split(',').map(phrase => phrase.trim());
-    if (phrases.some(phrase => employeeBranch.includes(phrase))) {
+    const phrases = filter.split(",").map((phrase) => phrase.trim());
+    if (phrases.some((phrase) => employeeBranch.includes(phrase))) {
       return true;
     }
   }
@@ -484,9 +445,65 @@ const isEmailNotEmployeeIdExists = async (emailAddress, employeeId) => {
   return data.Items.length > 0;
 };
 
+/////////////////////////////////////////////////////////////
+const getEmployeesByRole = async (event) => {
+  console.log("Get employees by role");
+  const response = {
+    statusCode: httpStatusCodes.SUCCESS,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+  try {
+    const { role } = event.queryStringParameters;
+
+    const params = {
+      TableName: process.env.EMPLOYEE_TABLE,
+      FilterExpression: "#role = :roleValue",
+      ExpressionAttributeNames: {
+        "#role": "role",
+      },
+      ExpressionAttributeValues: {
+        ":roleValue": { S: role },
+      },
+    };
+
+    const { Items } = await client.send(new ScanCommand(params));
+
+    const sortedItems = Items.sort((a, b) => parseInt(a.employeeId.N) - parseInt(b.employeeId.N));
+    const employees = sortedItems.map((item) => {
+      const employee = unmarshall(item);
+      return employee;
+    });
+    console.log({ employees });
+    if (!employees || !employees.length > 0) {
+      console.log("Employees details not found.");
+      response.statusCode = httpStatusCodes.NOT_FOUND;
+      response.body = JSON.stringify({
+        message: httpStatusMessages.EMPLOYEES_DETAILS_NOT_FOUND,
+      });
+    } else {
+      console.log("Successfully retrieved employee details.");
+      response.body = JSON.stringify({
+        message: httpStatusMessages.SUCCESSFULLY_RETRIEVED_EMPLOYEE_DETAILS,
+        data: employees,
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    response.body = JSON.stringify({
+      statusCode: e.statusCode,
+      message: httpStatusMessages.FAILED_TO_RETRIEVE_METADATA,
+      errorMsg: e.message,
+    });
+  }
+  return response;
+};
+
 module.exports = {
   createEmployee,
   updateEmployee,
   getEmployee,
   getAllEmployees,
+  getEmployeesByRole,
 };
