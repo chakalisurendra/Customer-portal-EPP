edfee467f236c01945386440a0ef212154765c31
surendra <surendra.chakali@hyniva.com>
2024-05-09 11:20:45 +0530

    new DB

--- src/Lamda/documents/todos.js
+++ src/Lamda/documents/todos.js
@@ -1,65 +1,93 @@
 const { Pool } = require("pg");
 
-const client = new Pool({
+// Create a connection pool
+const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  });
+});
 
 // Get all todos
 const getAllTodos = async (event) => {
   const query = "SELECT * FROM todos";
-  const { rows } = await client.query(query);
+  const { rows } = await pool.query(query);
   return rows;
 };
 
 // Get todo by ID
 const getTodoById = async (event) => {
-  console.log("SELECT * FROM todos WHERE id = $1");
+  const query = "SELECT * FROM todos";
+  console.log("query" + query);
+  const { rows } = await pool.query(query);
+  console.log("rows" + rows);
 
-  const xz = await client.connect((err) => {
-  if (err) {
-    console.error("Error connecting to PostgreSQL database:", err);
-  } else {
-    console.log("Connected to PostgreSQL database successfully.");
-  }
-});
-  // const query = "SELECT * FROM todos WHERE id = $1";
-  // const { rows } = await client.query(query, [id]);
-  console.log("after connection:  " + xz);
-  
-  return [];
+  return rows;
 };
 
-// Create a new todo
-const createTodo = async (event) => {
-  const { description, completed } = todo;
-  const query = "INSERT INTO todos (description, completed) VALUES ($1, $2) RETURNING *";
-  const { rows } = await client.query(query, [description, completed]);
-  return rows[0];
-};
+// const { Pool } = require("pg");
 
-// Update todo by ID
-const updateTodo = async (event) => {
-  const { description, completed } = updates;
-  const query = "UPDATE todos SET description = $1, completed = $2 WHERE id = $3 RETURNING *";
-  const { rows } = await client.query(query, [description, completed, id]);
-  return rows[0];
-};
+// const client = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   });
 
-// Delete todo by ID
-const deleteTodo = async (event) => {
-  const query = "DELETE FROM todos WHERE id = $1 RETURNING *";
-  const { rows } = await client.query(query, [id]);
-  return rows[0];
-};
+// // Get all todos
+// const getAllTodos = async (event) => {
+//   const query = "SELECT * FROM todos";
+//   const { rows } = await client.query(query);
+//   return rows;
+// };
+
+// // Get todo by ID
+// const getTodoById = async (event) => {
+//   console.log("SELECT * FROM todos WHERE id = $1");
+
+//   const xz = await client.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to PostgreSQL database:", err);
+//   } else {
+//     console.log("Connected to PostgreSQL database successfully.");
+//   }
+// });
+//   // const query = "SELECT * FROM todos WHERE id = $1";
+//   // const { rows } = await client.query(query, [id]);
+//   console.log("after connection:  " + xz);
+
+//   return [];
+// };
+
+// // Create a new todo
+// const createTodo = async (event) => {
+//   const { description, completed } = todo;
+//   const query = "INSERT INTO todos (description, completed) VALUES ($1, $2) RETURNING *";
+//   const { rows } = await client.query(query, [description, completed]);
+//   return rows[0];
+// };
+
+// // Update todo by ID
+// const updateTodo = async (event) => {
+//   const { description, completed } = updates;
+//   const query = "UPDATE todos SET description = $1, completed = $2 WHERE id = $3 RETURNING *";
+//   const { rows } = await client.query(query, [description, completed, id]);
+//   return rows[0];
+// };
+
+// // Delete todo by ID
+// const deleteTodo = async (event) => {
+//   const query = "DELETE FROM todos WHERE id = $1 RETURNING *";
+//   const { rows } = await client.query(query, [id]);
+//   return rows[0];
+// };
 
 module.exports = {
   getAllTodos,
   getTodoById,
-  createTodo,
-  updateTodo,
-  deleteTodo,
+  // createTodo,
+  // updateTodo,
+  // deleteTodo,
 };
